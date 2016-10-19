bae0033d94d0c4fa2c66cb33d560ee5d45eb931b
Zeick <timo.j.karkkainen@helsinki.fi>
2016-10-20 00:21:12 +0300

    Lisätty uusia virheilmoituksia. Syötteistä trimmataan whitespace pois.

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -17,15 +17,16 @@ import tikape.runko.domain.Alue;
 
 public class Main {
 
+    // Maksimipituudet foorumiin syötettäville teksteille.
+    // Final ja private, jottei sitä voida muuttaa kesken kaiken.
+    private static final int maxNimiPituus = 20;
+    private static final int maxOtsikkoPituus = 50;
+    private static final int maxKuvausPituus = 200;
+    private static final int maxViestiPituus = 9001; // Hehe :D
+
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:forum.db");
 
-        
-        //Tämä metodi luo automaattisesti Opiskelija -tietokannan.
-        // Katsokaa Database -luokasta sen toiminnallisuuta,
-        // olisi kiva jos joku automatisoisi Forum -tietokannan luomisen sinne.
-        // Mutta katsokaa siinä tapauksessa tarkkaan Forumin sarakkeiden nimet ja määrät,
-        // Ne eivät täsmää 100% raportissa määritellyihin CREATE TABLE -lauseihin.
         database.init();
 
         AlueDao alueDao = new AlueDao(database);
@@ -53,58 +54,96 @@ public class Main {
 
             return new ModelAndView(map, "alue");
         }, new ThymeleafTemplateEngine());
-        
+
         get("/alue/luo", (req, res) -> { //Hakee alueen luomis sivun. Tässä ei tapahdu mitään.
             HashMap map = new HashMap<>();
-            
 
             return new ModelAndView(map, "luoAlue");
         }, new ThymeleafTemplateEngine());
-        
+
         post("/alue/luo", (req, res) -> { // Luo uuden alueen
             HashMap map = new HashMap<>();
-            map.put("nimiPuuttuu", "Alueella täytyy olla nimi!");
             String nimi = req.queryParams("nimi");
             String kuvaus = req.queryParams("kuvaus");
-            if(nimi.isEmpty()) { // Jos nimikenttä on tyhjä, ladataan sivu uudestaan, tälläkertaa HashMapilla, jossa on "nimipuuttuu" olio.
-                return new ModelAndView(map,  "luoAlue");
+            nimi = nimi.trim();
+            kuvaus = kuvaus.trim();
+            // Jos nimikenttä on tyhjä, ladataan sivu uudestaan, tälläkertaa HashMapilla, jossa on "nimipuuttuu" olio.
+            if (nimi.isEmpty()) {
+                map.put("virheilmoitus", "Alueella täytyy olla nimi!");
+
+                return new ModelAndView(map, "luoAlue");
+            } // Sama, jos nimi on liian pitkä.
+            if (nimi.length() > maxOtsikkoPituus) {
+                map.put("virheilmoitus", "Alueen nimi voi olla korkeintaan " + maxOtsikkoPituus
+                        + " merkkiä!");
+                return new ModelAndView(map, "luoAlue");
+            }
+            if (kuvaus.length() > maxKuvausPituus) {
+                map.put("virheilmoitus", "Kuvauksen nimi voi olla korkeintaan " + maxKuvausPituus
+                        + " merkkiä!");
+                return new ModelAndView(map, "luoAlue");
             }
             alueDao.save(nimi, kuvaus);
             res.redirect("/alueet");
-            return new ModelAndView(map,  "luoAlue"); // Jos nimi on syötetty, niin tällä rivillä ei ole virkaa. Se vain estää punaisia alleviivauksia.
+            return new ModelAndView(map, "luoAlue"); // Jos nimi on syötetty, niin tällä rivillä ei ole virkaa. Se vain estää punaisia alleviivauksia.
         }, new ThymeleafTemplateEngine());
-        
+
         get("/alueet/:alueid/luo", (req, res) -> { // Hakee sivun jossa voi luoda uusia aiheita. TÄssä ei oikeastaan tapahdu mitään.
             HashMap map = new HashMap<>();
             map.put("takas", req.params("alueid"));
-            
 
             return new ModelAndView(map, "luoAihe");
         }, new ThymeleafTemplateEngine());
-        
-         post("/alueet/:alueid/luo", (req, res) -> { // Luo uuden aiheen valittuun alueeseen.
+
+        post("/alueet/:alueid/luo", (req, res) -> { // Luo uuden aiheen valittuun alueeseen.
             HashMap map = new HashMap<>();
             String viesti = req.queryParams("viesti");
             String nimi = req.queryParams("nimi");
             String kuvaus = req.queryParams("kuvaus");
             String kirjoittaja = req.queryParams("kirjoittaja");
-            if(nimi.isEmpty() && viesti.isEmpty()) { // Tarkistetaan täyttyvätkö ehdot. TÄTÄ VOISI MUOKATA TIETOKANNAN VARCHAR-MUUTTUJIEN MUKAISEKSI.
+            viesti = viesti.trim();
+            nimi = nimi.trim();
+            kuvaus = kuvaus.trim();
+            kirjoittaja = kirjoittaja.trim();
+            // Tarkistetaan ensimmäiseksi ovatko jotkut vaaditut kentät tyhjiä.
+            if (nimi.isEmpty() && viesti.isEmpty()) {
                 map.put("nimiviestiPuuttuu", "Aiheella täytyy olla nimi ja aloitusviesti!");
                 return new ModelAndView(map, "luoAihe");
-            } else if(nimi.isEmpty()) {
+            } else if (nimi.isEmpty()) {
                 map.put("nimiPuuttuu", "Aiheella täytyy olla nimi!");
-                return new ModelAndView(map,  "luoAihe");
-            } else if(viesti.isEmpty()) {
+                return new ModelAndView(map, "luoAihe");
+            } else if (viesti.isEmpty()) {
                 map.put("viestiPuuttuu", "Kirjoita aiheelle aloitusviesti!");
                 return new ModelAndView(map, "luoAihe");
             }
-            
+            // Seuraavaksi tarkistetaan ovatko jotkin kentät liian täysiä.
+            if(nimi.length() > maxOtsikkoPituus){
+                map.put("liianPitka","Aiheen otsikko voi olla korkeintaan " + maxOtsikkoPituus
+                + " merkkiä!");
+                return new ModelAndView(map,"luoAihe");
+            }
+            if(kirjoittaja.length() > maxNimiPituus){
+                map.put("liianPitka","Kirjoittajan nimi voi olla korkeintaan " + maxNimiPituus
+                + " merkkiä!");
+                return new ModelAndView(map,"luoAihe");
+            }
+            if(kuvaus.length() > maxKuvausPituus){
+                map.put("liianPitka","Kuvaus voi olla korkeintaan " + maxKuvausPituus
+                + " merkkiä!");
+                return new ModelAndView(map,"luoAihe");
+            }
+            if(viesti.length() > maxViestiPituus){
+                map.put("liianPitka","Viesti voi olla korkeintaan " + maxViestiPituus
+                + " merkkiä!");
+                return new ModelAndView(map,"luoAihe");
+            }
+
             aiheDao.save(Integer.parseInt(req.params("alueid")), nimi, kuvaus, kirjoittaja);
             viestiDao.save(kirjoittaja, viesti, aiheDao.getMaxId());
-            res.redirect("/alueet/" +req.params("alueid"));
-            return new ModelAndView(map,  "luoAihe");
+            res.redirect("/alueet/" + req.params("alueid"));
+            return new ModelAndView(map, "luoAihe");
         }, new ThymeleafTemplateEngine());
-         
+
         get("/aiheet/:id/:page", (req, res) -> {  // Hakee aiheessa näytettävät viestit
             HashMap map = new HashMap<>();
             map.put("aihe", aiheDao.findOne(Integer.parseInt(req.params("id"))));
@@ -115,12 +154,13 @@ public class Main {
             // asetetaan "next"in arvolle sama arvo kuin nykyisellä sivulla.
             // Jos sivunumero = 1, niin "prev"in arvoksi asetetaan 1, jotta sivunumero ei voi laskea nollaan ja sitä alemmas.
             int maxPage = viestiDao.maxPage(Integer.parseInt(req.params("id")));
-            if(Integer.parseInt(req.params("page"))< maxPage  ){
-                map.put("next", Integer.parseInt(req.params("page")) +1);
+            if (Integer.parseInt(req.params("page")) < maxPage) {
+                map.put("next", Integer.parseInt(req.params("page")) + 1);
             } else {
                 map.put("next", maxPage);
-            } if(Integer.parseInt(req.params("page")) > 1) {
-                map.put("prev", Integer.parseInt(req.params("page")) -1);
+            }
+            if (Integer.parseInt(req.params("page")) > 1) {
+                map.put("prev", Integer.parseInt(req.params("page")) - 1);
             } else {
                 map.put("prev", 1);
             }
@@ -130,25 +170,22 @@ public class Main {
 
             return new ModelAndView(map, "aihe");
         }, new ThymeleafTemplateEngine());
-        
+
         post("/aiheet/:id/:page", (req, res) -> { // Ottaa vastaan aiheeseen lähetettyä tietoa. Uusia viestejä, näytettävien viestin lkm.
-            if(req.queryParams("viesti") != null){ // if-lauseet pitävät huolen että data syötetään oikeassa muodossa.
+            if (req.queryParams("viesti") != null) { // if-lauseet pitävät huolen että data syötetään oikeassa muodossa.
                 String nimi = req.queryParams("nimi"); // täälläkin voisi muokata if-lauseita ottamaan huomioon tietokanna varChar vaatimukset.
                 String viesti = req.queryParams("viesti");
                 viestiDao.save(nimi, viesti, Integer.parseInt(req.params("id")));
             }
-            if(req.queryParams("lkm") != null) {
+            if (req.queryParams("lkm") != null) {
                 viestiDao.setLkm(Integer.parseInt(req.queryParams("lkm")));
-                res.redirect("/aiheet/" +Integer.parseInt(req.params("id")) +"/" +Integer.parseInt(req.params("page")));
+                res.redirect("/aiheet/" + Integer.parseInt(req.params("id")) + "/" + Integer.parseInt(req.params("page")));
             }
-            res.redirect("/aiheet/" +Integer.parseInt(req.params("id")) +"/" +viestiDao.maxPage(Integer.parseInt(req.params("id"))));
+            res.redirect("/aiheet/" + Integer.parseInt(req.params("id")) + "/" + viestiDao.maxPage(Integer.parseInt(req.params("id"))));
             return "";
-            
+
         });
-        
-        
-    }
-                
+
     }
 
-   
+}
